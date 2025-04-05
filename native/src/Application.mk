f6f4d024e102d250627e3ab49d9edd41dcab4ad8
1q23lyc45 <174172451+1q23lyc45@users.noreply.github.com>
2025-04-05 08:39:32 +0000

    Revert "Disable 16K page size (#42)"
    This reverts commit d5b60627f2a5b51a7adf0b0ec669d37e001ff4a5.

--- native/src/Application.mk
+++ native/src/Application.mk
@@ -7,11 +7,11 @@ APP_STL          := none
 APP_PLATFORM     := android-23
 APP_THIN_ARCHIVE := true
 APP_STRIP_MODE   := none
+APP_SUPPORT_FLEXIBLE_PAGE_SIZES := true
 
 # Busybox should use stock libc.a
 ifdef B_BB
 APP_PLATFORM     := android-26
-APP_LDFLAGS      += -T src/lto_fix.lds
 ifeq ($(OS),Windows_NT)
 APP_SHORT_COMMANDS := true
 endif
